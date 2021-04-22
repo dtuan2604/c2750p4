ff81400ac87f266ec3217f546712b3ffad3fcd27
Duong Tuan <tdhhxn@umsystem.edu>
2021-04-21 23:24:36 -0500

    Implement function executing options

--- calculator.c
+++ calculator.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 #include "function.h"
+
 
 int add(int first, int sec){
 	return 0;
@@ -20,7 +23,25 @@ int modulus(int first, int sec){
 
 
 int main(int argc, char** argv){
-
+	int option;
+	while( (option = getopt(argc, argv, "dh")) != -1){
+		switch(option){
+			case 'd':
+				freopen("log.txt","w", stderr);
+				break;
+			case 'h': 
+				help();
+				return 1;
+			case '?':
+				if( isprint(optopt))
+					fprintf(stderr, "Unknow option `-%c`.\n",optopt);
+				return EXIT_FAILURE;
+			default:
+				help();
+				return 1;
+		}
+	
+	}
 
 	return EXIT_SUCCESS;
 }
